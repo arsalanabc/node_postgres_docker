1ebd16bec2dae976f9584a8777070b672f235a92
arsalan.ahmed <arsalanabc@gmail.com>
2019-10-22 22:54:32 -0400

    Extract database lib to a separate module

--- app/server.js
+++ app/server.js
@@ -3,23 +3,6 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
-const { Client } = require('pg');
-
-const client = new Client({
-    user: process.env.POSTGRES_USER,
-    host: 'localhost',
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: 5432
-});
-
-client.connect();
-
-client.query('show tables;', (err, res) => {
-    console.log(res)
-    client.end()
-})
-
 const routes = require('./src/routes/routes.js')(app);
 
 app.listen(port, () => console.log('example of app listening'))
