3f716dd8952aa790c36e24ba689e47d1131469d4
arsalan.ahmed <arsalanabc@gmail.com>
2019-10-23 23:23:55 -0400

    Add logging to shoe controller and model

--- app/src/controllers/ShoeController.js
+++ app/src/controllers/ShoeController.js
@@ -1,32 +1,43 @@
 'use strict'
 const shoe = require('../models/Shoe');
+const logger = require('log4js').getLogger('ShoeController');
+const exceptionsLogger = require('log4js').getLogger('exceptions');
 
 const controller = {
     insert: (req, response, data) => {
+        logger.info('shoe/insert endpoint reached')
         shoe.insert({model: data.shoeModel})
             .then(data =>
                 data
                     ?response.send('Shoe model inserted')
-                    :response.status(500).send('Error: Shoe model was not inserted'))
-            .catch(err => console.log(err)); // catch promise rejection
+                    :response.status(500).send('Error: Shoe model was not inserted')
+            )
+            .catch(err => {
+                exceptionsLogger.error(err)
+            }); // catch promise rejection
     },
     get: (req, response) => {
+        logger.info('get endpoint reached')
          shoe.get()
              .then(data =>
                  data
                      ?response.send(data.rows)
                      :response.status(500).send('Error: Could\'nt get data'))
-             .catch(err => console.log(err)); // catch promise rejection
+             .catch(err => { // catch promise rejection
+                 exceptionsLogger.error(err)
+             });
     },
     addSize: (req, response, data) => {
+        logger.info('addsize endpoint reached')
         shoe.addModelSize(data)
             .then(data =>
                 data
                     ?response.send('Size inserted')
                     :response.status(500).send('Error: Size was not inserted'))
-            .catch(err => console.log(err)); // catch promise rejection
+            .catch(err => exceptionsLogger.log(err)); // catch promise rejection
     },
     trueToSize : (req, response, shoeModel) => {
+        logger.info('truetosize endpoint reached')
         shoe.trueToSizeCalculation(shoeModel)
             .then(data => {
                 return !data?response.send("Error: No data return"):data.rows[0]
@@ -35,7 +46,7 @@ const controller = {
                 data.avg!=null
                     ?response.send(`true to size for ${shoeModel} is ${data.avg}`)
                     :response.send("Shoe not found")})
-            .catch(err => console.log(err)) // catch promise rejection
+            .catch(err => exceptionsLogger.log(err)) // catch promise rejection
     }
 }
 
--- app/src/models/Shoe.js
+++ app/src/models/Shoe.js
@@ -1,57 +1,71 @@
 'use strict'
 const client = require('../db');
+const logger = require('log4js').getLogger('ShoeModel');
+const errorsLogger = require('log4js').getLogger('errors');
 
 const Shoe = {
     insert: async (shoe) => {
-            return await client.query(
-                'INSERT INTO shoes (model) VALUES ($1)',
+        const query = 'INSERT INTO shoes (model) VALUES ($1)';
+        logger.info(`Executing: ${query}`);
+        return await client.query(
+                query,
                 [shoe.model])
                 .then(res => {
+                    logger.info(`Query finished: ${query}`);
                     return res.rowCount>0?true:false;
                 })
                 .catch(e => {
-                    console.error(e);
+                    errorsLogger.fatal(e);
                     return false;
                 });
-    },
+        },
     get: async () => {
-        return await client.query('select * from shoes')
+        const query = 'select * from shoes';
+        logger.info(`Executing: ${query}`);
+        return await client.query(query)
             .then(res => {
+                logger.info(`Data returned for: ${query}`);
                 return res;
             })
             .catch(e => {
-                console.error(e);
+                errorsLogger.fatal(e);
                 return false;
             });
         },
-
     addModelSize: async (data) => {
+        const query = 'SELECT id from shoes WHERE model=$1 limit 1';
+        const insert_size_query = 'INSERT INTO size_data (shoe_model, size) VALUES ($1, $2)';
+        logger.info(`Executing: ${query}`);
         return await client.query(
-            'SELECT id from shoes WHERE model=$1 limit 1',
+             query,
             [data.shoeModel])
             .then(result => {
+                logger.info(`Data returned for: ${query}`);
                 const shoe_id = result.rows[0].id;
 
-                const insert_size_query = 'INSERT INTO size_data (shoe_model, size) VALUES ($1, $2)';
+                logger.info(`Executing: ${insert_size_query}`);
                 return client.query(insert_size_query,
                     [shoe_id, data.size])
             })
             .then(res => {
+                logger.info(`Query finished: ${insert_size_query}`);
                 return res.rowCount>0?true:false;
             })
             .catch(e => {
-                console.error(e);
+                errorsLogger.fatal(e);
                 return false;
             });
         },
     trueToSizeCalculation: async (shoeModel) => {
             const query = 'SELECT avg(sd.size) FROM size_data as sd JOIN shoes as s ON s.id=sd.shoe_model WHERE s.model=$1';
+            logger.info(`Executing: ${query}`);
             return await client.query(query, [shoeModel])
                 .then(res => {
+                    logger.info(`Data returned for: ${query}`);
                     return res;
                 })
                 .catch(e => {
-                    console.error(e);
+                    errorsLogger.fatal(e);
                     return false;
                 });
         }
