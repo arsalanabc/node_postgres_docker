1dbb87f66994ff4676ef8c1238119736690f10ca
arsalan.ahmed <arsalanabc@gmail.com>
2019-10-23 23:19:43 -0400

    Add log4js to the project and setup its config

--- app/server.js
+++ app/server.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000;
+
+const log4js = require('log4js');
+log4js.configure('./src/config/log4js.json');
+
+const log = log4js.getLogger();
 
 const routes = require('./src/routes/routes.js')(app);
 
-app.listen(port, () => console.log('example of app listening'))
+app.listen(port, () => console.log('example of app listening'));
+log.info('app launched and listening at', port);
 
 module.exports = app;
