257b3481b6c23b216482b01d47b929bf87573af4
arsalan.ahmed <arsalanabc@gmail.com>
2019-10-26 03:07:31 -0400

    Unit tests for Shoe Controller

--- app/src/controllers/ShoeController.js
+++ app/src/controllers/ShoeController.js
@@ -6,39 +6,45 @@ const exceptionsLogger = require('log4js').getLogger('exceptions');
 const controller = {
     insert: (req, response, data) => {
         logger.info('shoe/insert endpoint reached')
-        shoe.insert({model: data.shoeModel})
+        return shoe.insert({model: data.shoeModel})
             .then(data =>
                 data
                     ?response.send('Shoe model inserted')
-                    :response.status(500).send('Error: Shoe model was not inserted')
+                    :response.send('Error: Shoe model was not inserted')
             )
             .catch(err => {
                 exceptionsLogger.error(err)
+                response.status(500)
             }); // catch promise rejection
+
     },
     get: (req, response) => {
         logger.info('get endpoint reached')
-         shoe.get()
+         return shoe.get()
              .then(data =>
                  data
                      ?response.send(data.rows)
-                     :response.status(500).send('Error: Could\'nt get data'))
+                     :response.send('Error: Could\'nt get data'))
              .catch(err => { // catch promise rejection
                  exceptionsLogger.error(err)
+                 response.status(500);
              });
     },
     addSize: (req, response, data) => {
         logger.info('addsize endpoint reached')
-        shoe.addModelSize(data)
+        return shoe.addModelSize(data)
             .then(data =>
                 data
                     ?response.send('Size inserted')
-                    :response.status(500).send('Error: Size was not inserted'))
-            .catch(err => exceptionsLogger.log(err)); // catch promise rejection
+                    :response.send('Error: Size was not inserted'))
+            .catch(err => {
+                exceptionsLogger.log(err)
+                response.status(500);
+            }); // catch promise rejection
     },
     trueToSize : (req, response, shoeModel) => {
         logger.info('truetosize endpoint reached')
-        shoe.trueToSizeCalculation(shoeModel)
+        return shoe.trueToSizeCalculation(shoeModel)
             .then(data => {
                 return !data?response.send("Error: No data return"):data.rows[0]
             })
@@ -46,7 +52,9 @@ const controller = {
                 data.avg!=null
                     ?response.send(`true to size for ${shoeModel} is ${data.avg}`)
                     :response.send("Shoe not found")})
-            .catch(err => exceptionsLogger.log(err)) // catch promise rejection
+            .catch(err => {
+                exceptionsLogger.log(err)
+                response.status(500);}) // catch promise rejection
     }
 }
 
---
+++ app/test/unit/shoeController.test.js
@@ -0,0 +1,154 @@
+'use strict'
+const proxyquire =  require('proxyquire').noCallThru();
+const sinon = require('sinon');
+const chai = require('chai');
+const spies = require('chai-spies');
+chai.use(spies);
+const expect = chai.expect;
+
+const response = chai.spy();
+const testData = {shoeModel:'test'}
+chai.spy.on(response, ['send','status']);
+
+describe("ShoeController", () => {
+    const shoe = {insert:()=>{}} ;
+    const shoeController = proxyquire('../../src/controllers/ShoeController', {
+        '../models/Shoe': shoe
+    });
+
+    describe("should have all the routes", ()=>{
+        it('has the insert function', ()=>{
+            chai.expect(shoeController.insert).to.be.a('function');
+        });
+        it('has the truetosize function', ()=>{
+            chai.expect(shoeController.trueToSize).to.be.a('function');
+        });
+        it('has the addsize function', ()=>{
+            chai.expect(shoeController.addSize).to.be.a('function');
+        });
+        it('has the get function', ()=>{
+            chai.expect(shoeController.get).to.be.a('function');
+        });
+    });
+
+    describe("/insert",  ()=>{
+
+        it('calls Shoe insert with success', async () => {
+            sinon.stub(shoe, 'insert').returns(Promise.resolve(true));
+            chai.spy.on(shoe, ['insert']);
+
+            await shoeController.insert(()=>{}, response, testData)
+
+            expect(shoe.insert).to.have.been.called(1);
+            expect(shoe.insert).to.have.been.called.with({model: testData.shoeModel})
+            expect(response.send).to.have.been.called(1);
+        });
+
+        it('catches error', async () => {
+            sinon.stub(shoe, 'insert').returns(Promise.reject(false));
+            await shoeController.insert(()=>{}, response, testData)
+
+            expect(shoe.insert).to.have.been.called(1);
+            expect(shoe.insert).to.have.been.called.with({model: testData.shoeModel})
+            expect(response.send).to.have.been.called(1);
+            expect(response.status).to.have.been.called(1).with(500)
+        });
+    });
+    describe("/get",  ()=>{
+        shoe.get = () => {}
+        beforeEach(() => {
+            sinon.restore()
+        })
+
+        it('calls Shoe Get with success', async () => {
+            sinon.stub(shoe, 'get').returns(Promise.resolve(true));
+            chai.spy.on(shoe, ['get']);
+            chai.spy.on(shoe.get(), ['then']);
+
+            await shoeController.get(()=>{}, response)
+
+            expect(shoe.get).to.have.been.called(2);
+            expect(shoe.get().then).to.have.been.called(1);
+            expect(response.send).to.have.been.called(2);
+        });
+
+        it('calls Shoe get with failure', async () => {
+            sinon.stub(shoe, 'get').returns(Promise.resolve(false));
+            chai.spy.on(shoe, ['get']);
+            await shoeController.get(()=>{}, response, testData)
+
+            expect(shoe.get).to.have.been.called(1);
+            expect(response.send).to.have.been.called(3)
+        });
+    });
+
+    describe("/addSize",  ()=>{
+        shoe.addModelSize = () => {}
+        const testData = {shoeModel:'', size:''}
+        beforeEach(() => {
+            sinon.restore()
+        })
+        it('calls Shoe addModelSize with success', async () => {
+            sinon.stub(shoe, 'addModelSize').returns(Promise.resolve(true));
+            chai.spy.on(shoe, ['addModelSize']);
+
+            await shoeController.addSize(()=>{}, response, testData)
+
+            expect(shoe.addModelSize).to.have.been.called(1);
+            expect(shoe.addModelSize).to.have.been.called.with(testData)
+            expect(response.send).to.have.been.called(4);
+        });
+
+        it('calls Shoe addModelSize with failure', async () => {
+            sinon.stub(shoe, 'addModelSize').returns(Promise.reject(false));
+            chai.spy.on(shoe, ['addModelSize']);
+
+            await shoeController.addSize(()=>{}, response, testData)
+
+            expect(shoe.addModelSize).to.have.been.called(1);
+            expect(shoe.addModelSize).to.have.been.called.with(testData)
+            expect(response.send).to.have.been.called(4);
+            expect(response.status).to.have.been.called(2).with(500)
+        });
+    });
+
+    describe("/trueToSize",  ()=>{
+        shoe.trueToSizeCalculation = () => {}
+        beforeEach(() => {
+            sinon.restore()
+        })
+        it('calls Shoe trueToSizeCalculation with success', async () => {
+            sinon.stub(shoe, 'trueToSizeCalculation').returns(Promise.resolve(true));
+            chai.spy.on(shoe, ['trueToSizeCalculation']);
+
+            await shoeController.trueToSize(()=>{}, response, 'testData')
+
+            expect(shoe.trueToSizeCalculation).to.have.been.called(1);
+            expect(shoe.trueToSizeCalculation).to.have.been.called.with('testData')
+            expect(response.send).to.have.been.called(4);
+        });
+
+        it('calls trueToSizeCalculation for shoe not found', async () => {
+            sinon.stub(shoe, 'trueToSizeCalculation').returns(Promise.resolve(false));
+            chai.spy.on(shoe, ['trueToSizeCalculation']);
+
+            await shoeController.trueToSize(()=>{}, response, 'testData')
+
+            expect(shoe.trueToSizeCalculation).to.have.been.called(1);
+            expect(shoe.trueToSizeCalculation).to.have.been.called.with('testData')
+            expect(response.send).to.have.been.called(5);
+        });
+
+        it('calls Shoe trueToSizeCalculation with failure', async () => {
+            sinon.stub(shoe, 'trueToSizeCalculation').returns(Promise.reject(false));
+            chai.spy.on(shoe, ['trueToSizeCalculation']);
+
+            await shoeController.trueToSize(()=>{}, response, "testData")
+
+            expect(shoe.trueToSizeCalculation).to.have.been.called(1);
+            expect(shoe.trueToSizeCalculation).to.have.been.called.with("testData")
+            expect(response.send).to.have.been.called(5);
+            expect(response.status).to.have.been.called(5).with(500)
+        });
+    });
+});
